9c78b0cd08742643657f95fceee1f6b0ae8042d3
unknown <imranmalakzai>
2025-12-14 07:50:39 +0430

    fix the env configuration

--- src/config/env.js
+++ src/config/env.js
@@ -1,2 +1,4 @@
-import { configDotenv } from "dotenv";
-configDotenv();
+import dotenv from "dotenv";
+dotenv.configDotenv({ path: `.env.${"development" || "production"}.local` });
+
+export const { HOST_URI, DB_password, DB_USER, DB_NAME } = process.env;
