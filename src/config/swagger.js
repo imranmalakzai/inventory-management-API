a1d7fd593b19968b1d48aa21cecc56477684837f
unknown <imranmalakzai>
2025-12-19 06:30:14 +0430

    complete the swigger doc

---
+++ src/config/swagger.js
@@ -0,0 +1,34 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Inventory Management API",
+      version: "1.0.0",
+      description: "API documentation for Inventory System",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"], // Swagger reads comments here
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
